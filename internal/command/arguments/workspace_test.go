e293a6068e2d45e1d85f20548c9856b47205a5c3
Sarah French <sarah.french@hashicorp.com>
2026-04-22 15:46:51 +0100

    feat: Add `workspace show`-related code to arguments package

--- internal/command/arguments/workspace_test.go
+++ internal/command/arguments/workspace_test.go
@@ -93,3 +93,73 @@ func TestParseWorkspaceList_invalid(t *testing.T) {
 		})
 	}
 }
+
+func TestParseWorkspaceShow_valid(t *testing.T) {
+	testCases := map[string]struct {
+		args []string
+		want *WorkspaceShow
+	}{
+		"defaults": {
+			nil,
+			&WorkspaceShow{
+				Workspace: Workspace{
+					ViewType: ViewHuman,
+				},
+			},
+		},
+		"currently there is no validation about too many arguments": {
+			[]string{"bar"},
+			&WorkspaceShow{
+				Workspace: Workspace{
+					ViewType: ViewHuman,
+				},
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			got, diags := ParseWorkspaceShow(tc.args)
+			if len(diags) > 0 {
+				t.Fatalf("unexpected diags: %v", diags)
+			}
+			if *got != *tc.want {
+				t.Fatalf("unexpected result\n got: %#v\nwant: %#v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestParseWorkspaceShow_invalid(t *testing.T) {
+	testCases := map[string]struct {
+		args      []string
+		want      *WorkspaceShow
+		wantDiags tfdiags.Diagnostics
+	}{
+		"unknown flag": {
+			[]string{"-boop"},
+			&WorkspaceShow{
+				Workspace: Workspace{
+					ViewType: ViewHuman,
+				},
+			},
+			tfdiags.Diagnostics{
+				tfdiags.Sourceless(
+					tfdiags.Error,
+					"Failed to parse command-line flags",
+					"flag provided but not defined: -boop",
+				),
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			got, gotDiags := ParseWorkspaceShow(tc.args)
+			if *got != *tc.want {
+				t.Fatalf("unexpected result\n got: %#v\nwant: %#v", got, tc.want)
+			}
+			tfdiags.AssertDiagnosticsMatch(t, gotDiags, tc.wantDiags)
+		})
+	}
+}
